8beb114f33ba84d7e32b2e2a77583d54618b5616
rounakdatta <rounakdatta12@gmail.com>
2018-04-02 15:07:41 +0530

    closeGame, winnerAddress, getPrice

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -3,6 +3,9 @@ pragma solidity ^0.4.0;
 contract Lottery
 {
     address Owner;
+    address winner;
+    bool private closed = false;
+    
     modifier onlyowner()
     {
         if(msg.sender == Owner)
@@ -29,30 +32,61 @@ contract Lottery
        
     }
     
-    mapping(address => uint256) TokenBalances;
-    mapping(address => uint256) UserGuesses;
+    modifier closedOrNot()
+    {
+        if(closed != true)
+        {
+            _;
+        }
+        else
+        {
+            throw;
+        }
+    }
     
-    uint private winN;
+    modifier onlywinner()
+    {
+        if(msg.sender == winner)
+        {
+            _;
+        }
+        else
+        {
+            throw;
+        }
+    }
+    
+    mapping(address => uint256) TokenBalances;
+    mapping(bytes32 => address) UserGuesses;
+    
+    bytes32 private winN;
     
     function Lottery(uint winningNumber)
     {
-        Owner = msg.sender;
-        TokenBalances[Owner] = 1000000;
+        if(winningNumber >= 1 && winningNumber <= 1000000)
+        {
+            Owner = msg.sender;
+            TokenBalances[Owner] = 1000000;
         
-        winN = winningNumber;
+            winN = keccak256(winningNumber);
+        }
+        else
+        {
+            throw;
+        }
     }
     
     
-    function requestToken() payable onlyuser
+    function requestToken() payable onlyuser closedOrNot
     {
         uint value = msg.value;
         
-        uint ethToSend = value/ 1 ether;
+        uint ethAmount = value/ 1 ether;
         
-        if(TokenBalances[Owner] >= ethToSend)
+        if(TokenBalances[Owner] >= ethAmount)
         {
-            TokenBalances[Owner] -= ethToSend;
-            TokenBalances[msg.sender] += ethToSend;
+            TokenBalances[Owner] -= ethAmount;
+            TokenBalances[msg.sender] += ethAmount;
             
             if((value % 1 ether) != 0)
             {
@@ -63,21 +97,53 @@ contract Lottery
       
     }
     
-    function makeGuess(uint guess) payable 
+    function makeGuess(uint guess) payable closedOrNot
     {
         if(guess >= 1 && guess <= 1000000)
         {
             if(TokenBalances[msg.sender] > 0)
             {
-                UserGuesses[msg.sender] = guess;
                 TokenBalances[msg.sender] -= 1;
+                UserGuesses[keccak256(guess)] = msg.sender;
             }
+        }
+        else
+        {
+            throw;
         }
     }
     
-    function queryTokenBalance() public constant returns (uint256 myToken)
+    function closeGame() public payable onlyowner
     {
-        return TokenBalances[msg.sender];
+        closed = true;
+        winner = UserGuesses[winN];
+        
+        if(winner == 0x0000000000000000000000000000000000000000)
+        {
+            // if there is no winner, Owner will take call the ethers in the contract
+            Owner.transfer(this.balance);
+        }
+        
+    }
+    
+    function winnerAddress() constant returns (address winnerA)
+    {
+        return winner;
+    }
+    
+    function getPrice() payable onlywinner
+    {
+        if(closed == true)
+        {
+            uint halfTheReward = this.balance / 2;
+            
+            winner.transfer(halfTheReward);
+            Owner.transfer(halfTheReward);
+        }
+        else
+        {
+            throw;
+        }
     }
     
     function () payable
